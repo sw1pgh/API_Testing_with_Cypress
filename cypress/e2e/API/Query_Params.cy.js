788df27156830335c66b846f14fb366c4d688922
Swapnaneel Ghosh <ghoshswapnaneel2k1@gmail.com>
2026-05-03 11:15:47 +0530

    Query Parameters Learnt

--- cypress/e2e/API/Query_Params.cy.js
+++ cypress/e2e/API/Query_Params.cy.js
@@ -1,17 +1,26 @@
-describe.only("Practicing Query Parameters", () => {
-  const query_Parameter = { userId:1 };
-  const target_URL = "https://jsonplaceholder.typicode.com/posts"
+describe("Practicing Query Parameters", () => {
+  const query_Parameter = { userId: 1 };
+  const target_URL = "https://jsonplaceholder.typicode.com/posts";
 
-  it.only("Passing Query Paramters", () => {
+  it("Passing Query Paramters", () => {
     cy.request({
       method: "GET",
       url: target_URL,
       qs: query_Parameter,
     }).then((response) => {
       expect(response.status).to.eq(200);
-    //   expect(response.body.page).equal(2);
-    //   expect(response.data.length).has.length(6);
-    //   expect(response.body.data[0]).to.have.property("id", 7);
+      expect(response.body).has.length(10);
+      expect(response.body[0].userId).to.eq(1);
+    });
+  });
+
+  it("Rest Countries API", () => {
+    cy.request({
+      method: "GET",
+      url: "https://restcountries.com/v3.1/name/india",
+      qs: { fullText: true },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
     });
   });
 });
